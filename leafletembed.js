b82a96c9ea1096b297833af890298db1fc8a0209
Oli Thissen <oli@tonick.net>
2017-01-20 12:02:00 +0100

    Update leafletembed.js

--- leafletembed.js
+++ leafletembed.js
@@ -3,6 +3,8 @@ var ajaxRequest;
 var plotlist;
 var plotlayers=[];
 
+var feature = {}
+
 function onEachFeature(feature, layer) {
 	// does this feature have a property named popupContent?
 	if (feature.properties && feature.properties.prop) {
@@ -10,41 +12,29 @@ function onEachFeature(feature, layer) {
 	}
 }	
 
+function reloadData(map) {
+	$.getJSON( "https://raw.githubusercontent.com/olithissen/StolpersteineGangelt/master/map2.geojson", function(data) {
+		var myLayer = L.geoJSON().addTo(map);
+		myLayer.addData(data);
+		L.geoJSON(data, {
+			onEachFeature: onEachFeature
+		}).addTo(map);	
+		map.fitBounds(myLayer.getBounds());
+	});
+}
+
 function initmap() {
 	// set up the map
 	
-	var feature = {
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          5.997569561004638,
-          50.99288103999011
-        ]
-      },
-	  "properties" : {"prop" : "<b>foobar</b>: Fnord"}
-    }
-  ]
-}
-	
 	map = new L.Map('map');
-	var myLayer = L.geoJSON().addTo(map);
-	myLayer.addData(feature);
+	reloadData(map);
 
 	// create the tile layer with correct attribution
+	var osmAttrib = "foobar Openstreetmap";
 	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 19, attribution: osmAttrib});		
 
 	// start the map in South-East England
 	map.setView(new L.LatLng(51, 6),15);
 	map.addLayer(osm);
-	L.geoJSON(feature, {
-		onEachFeature: onEachFeature
-	}).addTo(map);	
-	map.fitBounds(myLayer.getBounds());
 }
