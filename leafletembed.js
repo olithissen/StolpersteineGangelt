81f43a67779cee8a8bd2da341fa35c34b6dad247
Oli Thissen <oli@tonick.net>
2017-01-22 22:22:53 +0100

    Erste Schritte für Template Engine

--- leafletembed.js
+++ leafletembed.js
@@ -1,33 +1,31 @@
 var map;
+var markers;
 var ajaxRequest;
 var plotlist;
 var plotlayers = [];
-
 var feature = {}
 
 function onEachFeature(feature, layer) {
     // does this feature have a property named popupContent?
     if (feature.properties && feature.properties.text) {
-        layer.bindPopup(feature.properties.text);
+        layer.bindPopup(`<div class="engraving">${feature.properties.text}</div>`);
     }
 }
 
-function reloadData(map) {
-    $.getJSON("https://raw.githubusercontent.com/olithissen/StolpersteineGangelt/master/map2.geojson", function(data) {
-        var myLayer = L.geoJSON().addTo(map);
-        myLayer.addData(data);
-        L.geoJSON(data, {
-            onEachFeature: onEachFeature
-        }).addTo(map);
-        map.fitBounds(myLayer.getBounds());
+function loadData() {
+    var layer = L.geoJSON(data, {
+        onEachFeature: onEachFeature
     });
+		markers.addLayer(layer);
+		map.addLayer(markers);
+    map.fitBounds(layer.getBounds(), {padding: [50, 50]});
 }
 
 function initmap() {
     // set up the map
 
     map = new L.Map('map');
-    reloadData(map);
+ 		markers = L.markerClusterGroup({spiderfyDistanceMultiplier: 2});
 
     // create the tile layer with correct attribution
     var osmAttrib = "foobar Openstreetmap";
@@ -41,4 +39,6 @@ function initmap() {
     // start the map in South-East England
     map.setView(new L.LatLng(51, 6), 15);
     map.addLayer(osm);
+
+    loadData(map);
 }
